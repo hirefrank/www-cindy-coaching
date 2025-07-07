67a764f6c919742415710e21b43fd965cda205d0
Frank Harris <frank@hirefrank.com>
2025-07-07 17:43:34 -0400

    yolo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import { copyFileSync } from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    react()
+    react(),
+    {
+      name: 'copy-worker',
+      hooks: {
+        'astro:build:done': () => {
+          copyFileSync('worker.js', 'dist/worker.js');
+        }
+      }
+    }
   ],
   vite: {
     resolve: {
